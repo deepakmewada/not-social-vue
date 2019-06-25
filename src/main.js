44fe350e65b93370100ada9b36fd81b8d717b92b
Arfat Bin kileb <arfatbk@gmail.com>
2019-06-25 13:47:15 +0530

    added Google Oauth2

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import GAuth from "vue-google-oauth2";
 
 Vue.config.productionTip = false;
 
+const gauthOption = {
+  clientId:
+    "306467913704-t29amabreflrk5h20tuv1bu0g7lr5hsi.apps.googleusercontent.com",
+  scope: "profile email",
+  prompt: "select_account"
+};
+Vue.use(GAuth, gauthOption);
 new Vue({
   router,
   store,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button type="button" @click="signIn">Sing in</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    signIn() {
+      console.log("here");
+      this.$gAuth
+        .getAuthCode()
+        .then(authCode => console.log(authCode))
+        .catch(err => console.error(err));
+    }
+  }
+};
+</script>
